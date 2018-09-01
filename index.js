4e52ddc3a0639ecf079a7b19175fb8b53b602592
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-01 10:23:25 +0100

    jo

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+  const token = '691df7830d0672964d89344494ca7031fe1c5e1a'
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
